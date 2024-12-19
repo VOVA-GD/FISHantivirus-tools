483d884a71dac1b4bd2c2002dca009a15a9e5888
VOVA-GD <vova63205@gmail.com>
2024-12-19 18:29:37 +0300

    1 файл
    это запускает cmd powershell taskmgr resmon regedit mrt control написано на powershell script и конвертировано с помощью PS2EXE

---
+++ start cmd powershell taskmgr resmon regedit mrt control.ps1
@@ -0,0 +1,8 @@
+﻿Start-process cmd.exe
+Start-process taskmgr.exe
+Start-process powershell.exe
+Start-process regedit.exe
+Start-process explorer.exe
+Start-process resmon.exe
+Start-process mrt.exe
+Start-process control.exe
